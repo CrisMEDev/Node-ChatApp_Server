295cb5ba98d416b47bdc1c9b4abe66cb564ee3ad
Cristian Morales <cr15moresp@gmail.com>
2021-07-09 02:36:23 -0500

    Almacenamiento en la base de datos con la contraseña encrptada

--- controllers/chat_auth.js
+++ controllers/chat_auth.js
@@ -1,16 +1,42 @@
 const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 
+const Usuario = require('../models/usuario');
+
 
 const crearUsuario = async( req = request, res = response ) => {
 
-    const { name, email, password } = req.body;
+    const { name, email, password, ...resto } = req.body;
+    
+    try {
 
-    res.json({
-        name,
-        email,
-        password
-    });
+        const existeEmail = await Usuario.findOne({ email });
+
+        if ( existeEmail ){
+            return res.status(400).json({ msg: 'Credenciales inválidas, intente de nuevo' });
+        }
+
+        if ( password ){
+            // Encriptar la contraseña
+            const salt = bcryptjs.genSaltSync();
+            resto.password = bcryptjs.hashSync( password, salt );
+        }
+
+        const usuario = new Usuario({ name, email, password: resto.password });
+        await usuario.save()
+        
+        res.json({
+            usuario
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            msg: 'Server error - hable con el administrador'
+        })
+    }
+
 
 }
 
---
+++ models/usuario.js
@@ -0,0 +1,54 @@
+// {
+//     nombre: '',
+//     correo: '',
+//     pass: '',       // Encriptado
+//     img: '',
+//     role: '',
+//     estado: false,  // Eliminado para los usuarios pero no de la base de datos
+//     google: true,   // Para saber si el usuario fue creado por google o el sistema propio del backend
+// }
+
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+        name: {
+            type: String,
+            required: [true, 'El nombre es obligatorio']    // El segundo es el mensaje de error que será mostrado en caso falte el nombre
+        },
+        
+        email: {
+            type: String,
+            required: [true, 'El correo es obligatorio'],
+            unique: true
+        },
+
+        password: {
+            type: String,
+            required: [true, 'La contraseña es obligatoria']
+        },       // Encriptado
+
+        online: {
+            type: Boolean,
+            default: false
+        },
+
+        state: {
+            type: Boolean,
+            default: true
+        },  // Eliminado para los usuarios pero no de la base de datos
+});
+
+UsuarioSchema.methods.toJSON = function(){
+
+    // Genera una instancia de mi Schema con sus valores respectivos
+    const { __v, online, state, password, _id, ...user } = this.toObject(); // Saca la version y el password de mi objeto Schema y el resto lo deja en user
+
+    user.uid = _id; // Agrega a la instancia de usuario nuevamente el id pero renombrado a uid
+
+    return user;
+}
+
+
+module.exports = model( 'Usuario', UsuarioSchema );
+
+
